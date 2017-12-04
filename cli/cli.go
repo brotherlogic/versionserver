0b93ba9a1aa6a39cc88629eaf7f309c3f28fb187
Simon Tucker <brotherlogic@gmail.com>
2017-12-04 15:33:46 -0800

    Added basic cli

---
+++ cli/cli.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/brotherlogic/goserver/utils"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	pb "github.com/brotherlogic/versionserver/proto"
+)
+
+func main() {
+     ip, port, err := utils.Resolve("versionserver")
+     if err != nil {
+     	log.Fatalf("Error resolving server: %v", err)
+		}
+	conn, _ := grpc.Dial(ip+":"+strconv.Itoa(int(port)), grpc.WithInsecure())
+				defer conn.Close()
+
+				registry := pb.NewVersionServerClient(conn)
+				answer, err := registry.GetVersion(context.Background(), &pb.GetVersionRequest{Key: os.Args[1]})
+				if err != nil {
+					log.Fatalf("Error reading version: %v", err)
+				}
+				log.Printf("Answer = %v", answer)
+			}
+		}
+	}
+}
